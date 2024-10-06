1bb0a9fbdcfeab3d31989f1d3e2f9fa06ea73bf8
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-10-06 11:36:01 +0200

    Add login functionality

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -4,6 +4,12 @@ import * as z from "zod";
 export const SignupValidation = z.object({
   name: z.string().min(2, { message: "Too short" }),
   username: z.string().min(2, { message: "Too short" }),
-  email: z.string().min(8, { message: "Too short" }),
+  email: z.string().email({ message: "Invalid email" }),
+  password: z.string().min(8, { message: "Too short" }),
+});
+
+//sign in
+export const SigninValidation = z.object({
+  email: z.string().email({ message: "Invalid email" }),
   password: z.string().min(8, { message: "Too short" }),
 });
