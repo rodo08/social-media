2ceb9d11ab207e8ae2830f17003616cd52a9d89c
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-10-14 11:43:56 +0200

    Add top bar buttons functionalities

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
-import { sign } from "crypto";
 import { useSignOutAccount } from "../../lib/react-query/queriesAndMutations";
+import { useUserContext } from "../../context/AuthContext";
 
 const Topbar = () => {
   const { mutate: signOut, isSuccess } = useSignOutAccount();
   const navigate = useNavigate();
+  const { user } = useUserContext();
 
   useEffect(() => {
     if (isSuccess) navigate(0);
@@ -28,10 +29,17 @@ const Topbar = () => {
           <Button
             variant="ghost"
             className="shad-button_ghost"
-            onClick={() => signOut}
+            onClick={() => signOut()}
           >
             <img src="/assets/icons/logout.svg" alt="logout" />
           </Button>
+          <Link to={`/profile/${user.id}`} className="flex-center gap-3">
+            <img
+              src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+              alt="profile"
+              className="w-8 h-8 rounded-full"
+            />
+          </Link>
         </div>
       </div>
     </section>
