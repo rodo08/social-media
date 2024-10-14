32ccf3ab6fba7b9f649d9d4520a9827a8ca79bd8
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-10-14 12:46:05 +0200

    Add sidebar + info

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
+import { useSignOutAccount } from "../../lib/react-query/queriesAndMutations";
+import { useUserContext } from "../../context/AuthContext";
 
 const LeftSidebar = () => {
-  return <div>LeftSidebar</div>;
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
+  return (
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex gap-3 items-center">
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={130}
+            height={325}
+          />
+        </Link>
+
+        <Link to={`/profile/${user.id}`}>
+          <img
+            src={user.imageUrl || "assets/icons/profile-placeholder.svg"}
+            alt="profile"
+            className="h-14 w-14 rounded-full"
+          />
+          <div className="flex flex-col">
+            <p className="body-bold">{user.name}</p>
+            <p className="small-regular text-light-3">@{user.username}</p>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default LeftSidebar;
